75636d25299d66398ffbdbaf3da4e078c8536683
Nayara <nrdeandrade@gmail.com>
2022-06-06 09:05:33 -0300

    fix: tratamento para dias negativos na questão c

--- 02-pt/c/index.js
+++ 02-pt/c/index.js
@@ -7,9 +7,13 @@ function clicar(e) {
     e.preventDefault();
     let gender = document.querySelector('input[name="gender"]:checked').value;
     let today = new Date();
+    console.log(today)
     let date = dateInput.value;
+    console.log(date)
     let birthDate = new Date(date);
+    console.log(birthDate)
     let age = today.getFullYear() - birthDate.getFullYear();
+    console.log(age)
     let checkMonth = today.getMonth() - birthDate.getMonth();
     if (
         checkMonth < 0 ||
@@ -25,10 +29,18 @@ function calcularMorte(age, gender) {
     let horasParaMorte = 0;
     if (gender === 'Masculino') {
         diasParaMorte = Math.floor((73.1 - age) * 365);
-        horasParaMorte = Math.round(((73.1 - age) * 365 - diasParaMorte) * 24);
+        horasParaMorte = Math.round(((73.1 - age) * 365 - diasParaMorte) * 24);        
     } else if (gender === 'Feminino') {
         diasParaMorte = Math.floor((80.1 - age) * 365);
         horasParaMorte = Math.round(((80.1 - age) * 365 - diasParaMorte) * 24);
+    } 
+    render(diasParaMorte, horasParaMorte);       
+}
+
+function render(diasParaMorte, horasParaMorte) {
+    if(diasParaMorte < 0) {
+        resultado.innerHTML = `Parabéns, você já viveu mais que a méidia nacional. seu saldo é de ${diasParaMorte * -1} dias`;       
+    } else {
+        resultado.innerHTML = `Você tem ${diasParaMorte} dias e ${horasParaMorte} horas até sua morte.`;
     }
-    resultado.innerHTML = `Você tem ${diasParaMorte} dias e ${horasParaMorte} horas até sua morte.`;
 }
