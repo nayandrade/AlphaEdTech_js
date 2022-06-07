55f0df18fbce4ea13edb9ce9e9d305be121a0ebf
Nayara <nrdeandrade@gmail.com>
2022-06-07 20:53:57 -0300

    chore: ajuste na atividade c e nos arquivos .CSS

--- 04-pt/d/index.js
+++ 04-pt/d/index.js
@@ -4,9 +4,9 @@ button.addEventListener('click', (e) => clicar(e));
 
 function clicar(e) {
     e.preventDefault();
-    const numero = Math.random() * (101) ;
+    const numero = Math.random() * 101;
     console.log(numero.toFixed(1));
-    if(numero.toFixed(1) < 8.6 && numero.toFixed(1) >= 0) {
+    if (numero.toFixed(1) < 8.6 && numero.toFixed(1) >= 0) {
         resultado.innerHTML = `Parabéns, você é uma senhorinha`;
     } else if (numero.toFixed(1) >= 8.6 && numero.toFixed(1) < 16.7) {
         resultado.innerHTML = `Parabéns, você é um senhor`;
@@ -14,8 +14,9 @@ function clicar(e) {
         resultado.innerHTML = `Parabéns, você é um senhor xovem, que escuta kpop`;
     } else {
         resultado.innerHTML = `Parabéns, você é uma senhorita xovem, que escuta kpop`;
-    }    
+    }
 }
-// M = 51.7% H = 48.3% 16.7% são idosos -> 
-// mulheres jovens = 43.0661% homens jovens = 40.2339%, 8.0661% de idosos homens e 8.6339% de mulheres
 
+
+// M = 51.7% H = 48.3% 16.7% são idosos ->
+// mulheres jovens = 43.0661% homens jovens = 40.2339%, 8.0661% de idosos homens e 8.6339% de mulheres
