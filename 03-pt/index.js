13f2a2a13624b48d26d53e5dfcf52e6bc2b9f502
Nayara <nrdeandrade@gmail.com>
2022-06-06 21:20:14 -0300

    chore: solução com objeto

--- 03-pt/index.js
+++ 03-pt/index.js
@@ -10,27 +10,44 @@ function clicar(e) {
     let primeiroValor = inputPrimeiroValor.value;
     let segundoValor = inputSegundoValor.value;
     let operator = inputOperator.value;
-    calcResult(primeiroValor, segundoValor, operator)
+    calcResult(primeiroValor, segundoValor, operator);
 }
 
+// PRIMEIRO EXEMPLO - COM USO DE eval()
 // function calcResult(primeiroValor, segundoValor, operator) {
-//     let resultadoFinal = eval(primeiroValor + operator + segundoValor);      
+//     let resultadoFinal = eval(primeiroValor + operator + segundoValor);
 //     resultado.innerHTML = `O resultado de sua operação é ${resultadoFinal}`;
 // }
 
+// SEGUNDO EXEMPLO - COM USO DE if/else
+// function calcResult(primeiroValor, segundoValor, operator) {
+//     let resultadoFinal = 0;
+//     if (operator === '+') {
+//         resultadoFinal = parseInt(primeiroValor) + parseInt(segundoValor);
+//     } else if (operator === '-') {
+//         resultadoFinal = parseInt(primeiroValor) - parseInt(segundoValor);
+//     } else if (operator === '*') {
+//         resultadoFinal = parseInt(primeiroValor) * parseInt(segundoValor);
+//     } else if (operator === '/') {
+//         resultadoFinal = parseInt(primeiroValor) / parseInt(segundoValor);
+//     }
+//     console.log(resultadoFinal);
+//     resultado.innerHTML = `O resultado de sua operação é ${resultadoFinal}`;
+// }
+
+// TERCEIRO EXEMPLO - COM USO DE DICTIONARY
 function calcResult(primeiroValor, segundoValor, operator) {
-    let resultadoFinal = 0
-    if (operator === '+') {
-        resultadoFinal = parseInt(primeiroValor) + parseInt(segundoValor);
-    } else if (operator === '-') {
-        resultadoFinal = parseInt(primeiroValor) - parseInt(segundoValor);
-    } else if (operator === '*') {
-        resultadoFinal = parseInt(primeiroValor) * parseInt(segundoValor);
-    } else if (operator === '/') {
-        resultadoFinal = parseInt(primeiroValor) / parseInt(segundoValor);
+    let resultadoFinal = 0;
+    const object = {
+        '+': (primeiroValor, segundoValor) => primeiroValor + segundoValor,
+        '-': (primeiroValor, segundoValor) => primeiroValor * segundoValor,
+        '/': (primeiroValor, segundoValor) => primeiroValor / segundoValor,
+        '*': (primeiroValor, segundoValor) => primeiroValor * segundoValor,
+    };
+    resultadoFinal = object[operator](parseInt(primeiroValor),parseInt(segundoValor));
+    if(resultadoFinal) {
+        resultado.innerHTML = `O resultado de sua operação é ${resultadoFinal}`;
+    } else {
+        resultado.innerHTML = `Escolha números válidos`;
     }
-    console.log(resultadoFinal);
-    resultado.innerHTML = `O resultado de sua operação é ${resultadoFinal}`;
 }
-
-
