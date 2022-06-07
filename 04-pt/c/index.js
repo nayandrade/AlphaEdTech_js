73878a5d4b40c85d5efffbe73a07336dd3130a1e
Nayara <nrdeandrade@gmail.com>
2022-06-07 20:30:52 -0300

    feat atividade 4c

--- 04-pt/c/index.js
+++ 04-pt/c/index.js
@@ -1,51 +1,22 @@
 const button = document.querySelector('button');
-const inputPeso = document.querySelector('#peso');
-const inputAltura = document.querySelector('#altura');
+const inputNumber = document.querySelector('#number');
 const resultado = document.querySelector('h1');
 button.addEventListener('click', (e) => clicar(e));
 
 function clicar(e) {
-    e.preventDefault();
-    let peso = inputPeso.value;
-    let altura = inputAltura.value;
-    handdleData(peso, altura);
+    e.preventDefault(); 
+    let myNumber = inputNumber.value;
+    handdleData(myNumber);
 }
 
-function handdleData(peso, altura) {
-    peso.indexOf(',') > -1 ? (peso = peso.replace(',', '.')) : peso;
-    altura.indexOf(',') > -1 ? (altura = altura.replace(',', '.')) : altura;
-    if (isNaN(peso) || isNaN(altura)) {
-        resultado.innerHTML = 'Preencha todos os campos de forma válida';
-    }
-    calcResult(peso, altura);
-}
-
-function calcResult(peso, altura) {
-    let imc = 0;
-    imc = peso / (altura * altura);
-    if (imc < 18.5) {
-        resultado.innerHTML = `Seu IMC é ${imc.toFixed(
-            2
-        )}, você está abaixo do peso`;
-    } else if (imc >= 18.5 && imc < 24.9) {
-        resultado.innerHTML = `Seu IMC é ${imc.toFixed(
-            2
-        )}, você está com peso normal`;
-    } else if (imc >= 25 && imc < 29.9) {
-        resultado.innerHTML = `Seu IMC é ${imc.toFixed(
-            2
-        )}, você está com sobrepeso`;
-    } else if (imc >= 30 && imc < 34.9) {
-        resultado.innerHTML = `Seu IMC é ${imc.toFixed(
-            2
-        )}, você está com obesidade grau 1`;
-    } else if (imc >= 35 && imc < 39.9) {
-        resultado.innerHTML = `Seu IMC é ${imc.toFixed(
-            2
-        )}, você está com obesidade grau 2`;
-    } else if (imc >= 40) {
-        resultado.innerHTML = `Seu IMC é ${imc.toFixed(
-            2
-        )}, você está com obesidade grau 3`;
-    }
+function handdleData(myNumber) {
+    myNumber.indexOf(',') > -1 ? (myNumber = myNumber.replace(',', '.')) : myNumber;
+    myNumber = parseFloat(myNumber);
+    let maxValue = Math.floor(myNumber);
+    let minValue = Math.ceil(myNumber);
+    if (isNaN(myNumber) || Number.isInteger(myNumber) ) {
+        resultado.textContent = 'Valores inválidos';
+    } else {
+        resultado.innerHTML = `Seu número inteiro máximo é ${maxValue} e o mínimo é ${minValue}`;
+    }    
 }
