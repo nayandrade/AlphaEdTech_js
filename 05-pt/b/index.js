6da753f573886c7b4b7c137aaa70382690403cbd
Nayara <nrdeandrade@gmail.com>
2022-06-08 17:28:29 -0300

    feat: atividade 5b

--- 05-pt/b/index.js
+++ 05-pt/b/index.js
@@ -1,47 +1,108 @@
 const button = document.querySelector('button');
-const valueInput = document.querySelector('#number');
+const dateInput = document.querySelector('#date');
 const resultado = document.querySelector('h1');
 button.addEventListener('click', (e) => clicar(e));
 
 function clicar(e) {
     e.preventDefault();
-    if (valueInput.value >= 0 && valueInput.value <= 10) {
-        switch (valueInput.value) {
-            case '0':
-                resultado.innerHTML = 'Zero';
-                break;
-            case '1':
-                resultado.innerHTML = 'Um';
-                break;
-            case '2':
-                resultado.innerHTML = 'Dois';
-                break;
-            case '3':
-                resultado.innerHTML = 'Três';
-                break;
-            case '4':
-                resultado.innerHTML = 'Quatro';
-                break;
-            case '5':
-                resultado.innerHTML = 'Cinco';
-                break;
-            case '6':
-                resultado.innerHTML = 'Seis';
-                break;
-            case '7':
-                resultado.innerHTML = 'Sete';
-                break;
-            case '8':
-                resultado.innerHTML = 'Oito';
-                break;
-            case '9':
-                resultado.innerHTML = 'Nove';
-                break;
-            case '10':
-                resultado.innerHTML = 'Dez';
-                break;
-        }
-    } else {
-        resultado.innerHTML = `Insira um valor entre 0 e 10 para ser validado`;
+    const date = new Date(dateInput.value);  
+    const day = date.getUTCDate();
+    const month = date.getUTCMonth();
+    const year = date.getUTCFullYear();
+    const weekDay = date.getUTCDay();
+    const dateZero = Date.parse('01 Jan 1970 00:00:00 GMT');
+    const selectedDateMiliSeconds = Date.parse(date);
+    let weekDayName = '';
+    let monthName = '';
+
+    switch (weekDay) {
+        case 0:
+            weekDayName = 'Domingo';
+            break;
+        case 1:
+            weekDayName = 'Segunda-feira';
+            break;
+        case 2:
+            weekDayName = 'Terça-feira';
+            break;
+        case 3:
+            weekDayName = 'Quarta-feira';
+            break;
+        case 4:
+            weekDayName = 'Quinta-feira';
+            break;
+        case 5:
+            weekDayName = 'Sexta-feira';
+            break;
+        case 6:
+            weekDayName = 'Sábado';
+            break;
     }
+
+    switch (month) {
+        case 0:
+            monthName = 'Janeiro';
+            break;
+        case 1:
+            monthName = 'Fevereiro';
+            break;
+        case 2:
+            monthName = 'março';
+            break;
+        case 3:
+            monthName = 'Abril';
+            break;
+        case 4:
+            monthName = 'Maio';
+            break;
+        case 5:
+            monthName = 'Junho';
+            break;
+        case 6:
+            monthName = 'Julho';
+            break;
+        case 7:
+            monthName = 'Agosto';
+            break;
+        case 8:
+            monthName = 'Setembro';
+            break;
+        case 9:
+            monthName = 'Outubro';
+            break;
+
+        case 10:
+            monthName = 'Novembro';
+            break;
+        case 11:
+            monthName = 'Dezembro';
+            break;
+    }
+    console.log(dateInput.value, date, day, month, year, selectedDateMiliSeconds, dateZero);
+    render(
+        day,
+        month,
+        year,
+        weekDayName,
+        monthName,
+        selectedDateMiliSeconds
+    );
+}
+
+function render(
+    day,
+    month,
+    year,
+    weekDayName,
+    monthName,
+    selectedDateMiliSeconds
+) {
+    resultado.innerHTML = `
+        <p>Dia selecionado: ${day}</p>
+        <p>Mês selecionado: ${month}</p>
+        <p>Ano selecionado: ${year}</p>
+        <p>Dia da semana em português: ${weekDayName}</p>
+        <p>Mês em português: ${monthName}</p>
+        <p>Data em milisegundos: ${selectedDateMiliSeconds}</p>
+    `;
 }
