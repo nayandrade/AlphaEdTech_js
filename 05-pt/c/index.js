5c158dd18260f7e2008b38aa14e2989026fa486a
Nayara <nrdeandrade@gmail.com>
2022-06-08 18:31:20 -0300

    chore: Ajuste dos links para os produtos na atividade 5c

--- 05-pt/c/index.js
+++ 05-pt/c/index.js
@@ -8,19 +8,22 @@ function clicar(e) {
     let product = ''    
     switch (valueInput.value) {
         case 'product1':
-            product = './assets/img/chinelo-abridor-de-garrafas.webp';
-            break;
-        case 'product2':
             product = 'https://raw.githubusercontent.com/nayandrade/AlphaEdTech_js/main/05-pt/c/assets/images/iluminador-de-livro.webp';
             break;
+        case 'product2':
+            product = 'https://raw.githubusercontent.com/nayandrade/AlphaEdTech_js/main/05-pt/c/assets/images/metralhadora-de-marshmallow.webp';
+            break;
         case 'product3':
-            product = 'https://blog.vindi.com.br/wp-content/uploads/2013/08/produtos-criativo-abridor-de-garrafas-cerveja.jpg';
+            product = 'https://raw.githubusercontent.com/nayandrade/AlphaEdTech_js/main/05-pt/c/assets/images/chinelo-abridor-de-garrafas.webp';
             break;
         case 'product4':
-            product = 'https://blog.vindi.com.br/wp-content/uploads/2013/08/produtos-criativos-saia-pulando.jpeg';
+            product = 'https://raw.githubusercontent.com/nayandrade/AlphaEdTech_js/main/05-pt/c/assets/images/sapato-canguru.webp';
             break;
         case 'product5':
-            product = 'https://blog.vindi.com.br/wp-content/uploads/2013/08/produtos-criativos5.png';
+            product = 'https://raw.githubusercontent.com/nayandrade/AlphaEdTech_js/main/05-pt/c/assets/images/toca-de-pipoca.webp';
+            break;
+        case 'Escolha um Produto':
+            product = 'Escolha um produto';
             break;
     }
     render(product)
@@ -28,9 +31,13 @@ function clicar(e) {
 
 function render(product) {
     console.log(product)
-    resultado.innerHTML = `
-    <div class="image-container">
-        <div style="background-image: url(${product})"></div>   
-    </div>
-    `
+    if (product === 'Escolha um produto') {
+        resultado.innerHTML = `<h1>${product}</h1>`
+    } else {
+        resultado.innerHTML = `
+            <div class="image-container">
+                <div style="background-image: url(${product})"></div>   
+            </div>
+            `
+    }    
 }
