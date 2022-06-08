d7d416049304b7fe413f477ca05096170c3d77dc
Nayara <nrdeandrade@gmail.com>
2022-06-08 08:59:42 -0300

    chore: ajuste nos parâmetros para o resultado da questão 4d

--- 04-pt/d/index.js
+++ 04-pt/d/index.js
@@ -4,16 +4,16 @@ button.addEventListener('click', (e) => clicar(e));
 
 function clicar(e) {
     e.preventDefault();
-    const numero = Math.random() * 101;
-    console.log(numero.toFixed(1));
-    if (numero.toFixed(1) < 8.6 && numero.toFixed(1) >= 0) {
+    const numero = Math.round(Math.random() * 10000);
+    console.log(numero);
+    if (numero <= 863 && numero > 0) {
         resultado.innerHTML = `Você é aleatoriamente uma idosa`;
-    } else if (numero.toFixed(1) >= 8.6 && numero.toFixed(1) < 16.7) {
+    } else if (numero > 863 && numero <= 1670) {
         resultado.innerHTML = `Você é aleatoriamente um idoso`;
-    } else if (numero.toFixed(1) >= 16.7 && numero.toFixed(1) < 56.9) {
-        resultado.innerHTML = `Você é aleatoriamente um homem jovem`;
-    } else {
+    } else if (numero > 1670 && numero <= 5977) {
         resultado.innerHTML = `Você é aleatoriamente uma mulher jovem`;
+    } else {
+        resultado.innerHTML = `Você é aleatoriamente um homem jovem`;
     }
 }
 
