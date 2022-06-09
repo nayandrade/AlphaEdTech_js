8188a4ca11a167b84b76dbb8ee637b191db63ddb
Nayara <nrdeandrade@gmail.com>
2022-06-09 14:08:32 -0300

    feat: atividade 6b

---
+++ 06-pt/b/index.js
@@ -0,0 +1,55 @@
+const bomb = document.querySelector('.bomb');
+const resultado = document.querySelector('section');
+const armarBomba = document.querySelector('.armar');
+const meusSegundos = 60;
+const umSegundo = 1000
+let acesa = false;
+let tokenSetInterval = null;
+
+armarBomba.addEventListener('click', init);
+bomb.addEventListener('click', desarmarBomba);
+
+function init() {
+    bomb.style.backgroundImage = 'url(./assets/images/bomba.png)';
+    document.querySelector('h2').innerHTML = 'A bomba foi plantada!';    
+    document.querySelector('.faca-se-a-luz').classList.remove('none');
+    bomb.classList.add('acesa');
+    acesa = true
+    cronometro();
+}
+
+
+function desarmarBomba() {
+    if (acesa) {
+        clearInterval(tokenSetInterval);
+        bomb.classList.remove('acesa');
+        document.querySelector('h2').innerHTML = 'Ufa, bomba desarmada!';
+    }
+}
+
+function explodirBomba() {
+    clearInterval(tokenSetInterval);
+    const explosao = document.querySelector('#explosao');
+    document.querySelector('h2').innerHTML = 'Explodiu';
+    bomb.classList.remove('acesa');
+    bomb.style.backgroundImage = 'url(./assets/images/explosion.png)';
+    explosao.play();
+    acesa = false;
+}
+
+function cronometro() {
+    let tempoSegundos = meusSegundos;
+    const segundos = document.querySelector('.segundos');
+    const ticCronometro = document.querySelector('#tic');
+
+    tokenSetInterval = setInterval(function () {
+        if (tempoSegundos > 0) {
+            tempoSegundos--
+            console.log(tempoSegundos)
+            segundos.innerHTML = `${tempoSegundos}s`
+            ticCronometro.play();
+        } else {
+            explodirBomba()
+        }    
+    }, umSegundo)
+}
